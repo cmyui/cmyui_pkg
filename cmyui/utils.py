41d1aac10ee1df1b21de5b1fad471426c4a0df5c
cmyui <cmyuiosu@gmail.com>
2020-11-14 20:45:55 -0500

    add unbounded async function cache

--- cmyui/utils.py
+++ cmyui/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from collections import namedtuple
+from functools import update_wrapper
 from string import ascii_letters, digits
 from random import choice
 from datetime import (
@@ -8,7 +10,7 @@ from datetime import (
     timedelta as td
 )
 
-__all__ = ('get_timestamp', '_isdecimal', 'rstring')
+__all__ = ('get_timestamp', '_isdecimal', 'rstring', 'async_cache')
 
 ts_fmt = ('%I:%M:%S%p', '%d/%m/%Y %I:%M:%S%p')
 tz_est = tz(td(hours = -4), 'EDT')
@@ -28,3 +30,50 @@ def _isdecimal(s: str, _float: bool = False,
 __chars = ascii_letters + digits
 def rstring(l: int, seq: str = __chars) -> str:
     return ''.join((choice(seq) for _ in range(l)))
+
+_CacheStats = namedtuple('CacheStats', ['hits', 'misses'])
+
+def _make_key(args, kwargs):
+    """Get a hashed value out of args & kwargs."""
+    key = args
+
+    if kwargs:
+        for item in kwargs.items():
+            key += item
+
+    return hash(key)
+
+def async_cache(f):
+    """Lightweight unbounded async function cache."""
+    cache = {}
+    hits = misses = 0
+
+    async def wrapper(*args, **kwargs):
+        key = _make_key(args, kwargs)
+
+        if key in cache:
+            nonlocal hits
+            hits += 1
+
+            val = cache[key]
+        else:
+            nonlocal misses
+            misses += 1
+
+            val = await f(*args, **kwargs)
+            cache[key] = val
+
+        return val
+
+    def cache_clear():
+        nonlocal hits, misses
+        hits = misses = 0
+        cache.clear()
+
+    def cache_stats():
+        nonlocal hits, misses
+        return _CacheStats(hits, misses)
+
+    wrapper.cache_clear = cache_clear
+    wrapper.cache_stats = cache_stats
+    return update_wrapper(wrapper, f)
